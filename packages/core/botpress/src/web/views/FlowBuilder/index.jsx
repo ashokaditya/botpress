aca974cc3afb27a142a71d0360be09894e1e44e4
Eugene Mirotin <emirotin@gmail.com>
2018-06-14 19:41:09 +0500

    fix(core): fix for the user with id === 0

--- packages/core/botpress/src/web/views/FlowBuilder/index.jsx
+++ packages/core/botpress/src/web/views/FlowBuilder/index.jsx
@@ -24,7 +24,7 @@ class FlowBuilder extends Component {
   }
 
   init() {
-    if (this.state.initialized || !this.props.user || !this.props.user.id) {
+    if (this.state.initialized || !this.props.user || this.props.user.id == null) {
       return
     }
     this.setState({
